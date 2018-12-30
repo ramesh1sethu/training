c995e1857189dc675a680da5ed7540ea5558a835
Ramesh.Sethumadhavan <Ramesh.Sethumadhavan@MLBTAL-13027.in.marlabs.com>
2018-12-30 22:39:35 +0530

    Initial commit.

---
+++ hello-world/src/main/java/com/marlabs/training/demo/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.marlabs.training.demo;
+import java.util.logging.*;
+
+public class HelloWorld {
+
+	int a = 0;
+	public int b = 3;
+	//	static Logger logger = Logger.getLogger("myLogger");
+	public static void main(String[] args) {
+		System.out.println("Hello World program started.");
+
+		for (int b = 0; b<5; b++) {
+			System.out.println("Values are " + b);
+		}
+
+	}
+
+}
