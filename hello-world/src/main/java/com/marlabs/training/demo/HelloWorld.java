35b89608ec53dedcfc6e791753b3c9a91de72121
CVRAMAN-491 <CVRAMAN-491@CVRAMAN-491>
2019-01-03 16:43:52 +0530

    Changes for sonar lint

--- hello-world/src/main/java/com/marlabs/training/demo/HelloWorld.java
+++ hello-world/src/main/java/com/marlabs/training/demo/HelloWorld.java
@@ -1,16 +1,13 @@
 package com.marlabs.training.demo;
 import java.util.logging.*;
-
 public class HelloWorld {
 
-	int a = 0;
-	public int b = 3;
-	//	static Logger logger = Logger.getLogger("myLogger");
+	private static  Logger logger = Logger.getLogger("myLogger");
 	public static void main(String[] args) {
-		System.out.println("Hello World program started.");
+		logger.info("Hello World program started.");
 
 		for (int b = 0; b<5; b++) {
-			System.out.println("Values are " + b);
+			logger.log(Level.INFO,"Values are: {0}",b);
 		}
 
 	}
