667d54c2a42e36ecb57145775935510936ff505c
CVRAMAN-531 <CVRAMAN-531@192.168.40.31>
2019-01-03 16:45:05 +0530

    Training demo:made some variable changes and logic.

--- hello-world/src/main/java/com/marlabs/training/demo/HelloWorld.java
+++ hello-world/src/main/java/com/marlabs/training/demo/HelloWorld.java
@@ -1,16 +1,16 @@
 package com.marlabs.training.demo;
+
 import java.util.logging.*;
 
 public class HelloWorld {
 
-	int a = 0;
-	public int b = 3;
-	//	static Logger logger = Logger.getLogger("myLogger");
-	public static void main(String[] args) {
-		System.out.println("Hello World program started.");
+	public static final Logger logger = Logger.getLogger("HelloWorld.class");
 
-		for (int b = 0; b<5; b++) {
-			System.out.println("Values are " + b);
+	public static void main(String[] args) {
+		logger.log(Level.INFO, "Hello World program started.");
+
+		for (int ctr = 0; ctr < 5; ctr++) {
+			logger.log(Level.INFO, "Values are {0}", ctr);
 		}
 
 	}
