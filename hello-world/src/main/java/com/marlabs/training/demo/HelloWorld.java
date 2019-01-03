bbd471b839e322e1e93eccf872c093479260d715
CVRAMAN-535 <CVRAMAN-535@192.168.40.51>
2019-01-03 17:45:32 +0530

    marlabs06

--- hello-world/src/main/java/com/marlabs/training/demo/HelloWorld.java
+++ hello-world/src/main/java/com/marlabs/training/demo/HelloWorld.java
@@ -1,20 +1,29 @@
 package com.marlabs.training.demo;
 
-
-import java.util.logging.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 //This is a demo session.
+/**
+ * @author CVRAMAN-535
+ *
+ */
 public class HelloWorld {
 
+	/**
+	 * 
+	 */
 	public static final Logger logger = Logger.getLogger("HelloWorld.class");
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		logger.log(Level.INFO, "Hello World program started.");
 
 		for (int ctr = 0; ctr > -5; ctr--) {
 			logger.log(Level.INFO, "Value is {0}", ctr);
 			logger.log(Level.INFO, "This is sushanta's change");
-
 
 		}
 
