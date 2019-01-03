bc21ae495b44a35959fdcc0390863eaba0e8bf44
CVRAMAN-482 <CVRAMAN-482@192.168.40.48>
2019-01-03 16:34:57 +0530

    adding one more class and giving proper comment

--- hello-world/src/main/java/com/marlabs/training/demo/HelloWorld.java
+++ hello-world/src/main/java/com/marlabs/training/demo/HelloWorld.java
@@ -3,10 +3,20 @@ package com.marlabs.training.demo;
 import java.util.logging.*;
 
 //This is a demo session.
+/**
+ * @author CVRAMAN-482
+ *
+ */
 public class HelloWorld {
 
+	/**
+	 * Logger
+	 */
 	public static final Logger logger = Logger.getLogger("HelloWorld.class");
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		logger.log(Level.INFO, "Hello World program started.");
 
---
+++ hello-world/src/main/java/com/marlabs/training/demo/MyClass.java
@@ -0,0 +1,11 @@
+package com.marlabs.training.demo;
+
+/**
+ * @author CVRAMAN-482
+ *
+ */
+public class MyClass {
+	
+	
+
+}
